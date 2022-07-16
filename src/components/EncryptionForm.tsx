fcaba2bcd1fcdfae06b0a1ea2dd9b6e994550ca3
Victor Nogueira <felladrin@gmail.com>
2022-07-16 22:22:09 +0000

    Clean code

--- src/components/EncryptionForm.tsx
+++ src/components/EncryptionForm.tsx
@@ -1,77 +1,48 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Input, Button, Message, Icon } from "semantic-ui-react";
-import { useStore } from "effector-react";
-import { createEffect, createEvent, restore, createStore, sample, forward, guard } from "effector";
 import { bcrypt } from "hash-wasm";
 
-function getRandomUint8Array(length: number) {
-  const salt = new Uint8Array(length);
-  window.crypto.getRandomValues(salt);
-  return salt;
-}
-
-const selectAndCopyValueFromInputElement = createEffect((inputElement: HTMLInputElement) => {
-  inputElement.select();
-  inputElement.setSelectionRange?.(0, inputElement.value.length);
-  document.execCommand("copy");
-});
-
-const encrypt = createEffect((password: string) =>
-  bcrypt({
-    password,
-    costFactor: 10,
-    salt: getRandomUint8Array(16),
-    outputType: "encoded",
-  })
-);
-
-const encryptButtonClicked = createEvent();
-
-const encryptionResultMessageDismissed = createEvent();
-
-const copyButtonClicked = createEvent();
-
-const textToEncryptUpdated = createEvent<string>();
-
-const copyInputReferenceUpdated = createEvent<Input | null>();
-
-const textToEncryptStore = restore(textToEncryptUpdated, "");
-
-const encryptedTextStore = restore(encrypt.doneData, "");
-
-const displayEncryptionResultStore = restore(
-  encryptedTextStore.map((encryptedText) => encryptedText.length > 0).updates,
-  false
-).reset([encryptionResultMessageDismissed, textToEncryptStore.updates]);
-
-const copyButtonClickedStore = createStore(false)
-  .on(copyButtonClicked, () => true)
-  .reset(encryptedTextStore.updates);
-
-const encryptButtonDisabledStore = textToEncryptStore.map((textToEncrypt) => textToEncrypt.length === 0);
-
-const copyInputStore = restore(copyInputReferenceUpdated, null);
-
-sample({
-  clock: encryptButtonClicked,
-  source: textToEncryptStore,
-  target: encrypt,
-});
-
-forward({
-  from: guard({
-    clock: copyButtonClicked,
-    source: copyInputStore,
-    filter: (copyInput): copyInput is Input => copyInput !== null,
-  }).map((copyInput) => (copyInput as unknown as { inputRef: { current: HTMLInputElement } }).inputRef.current),
-  to: selectAndCopyValueFromInputElement,
-});
-
 export function EncryptionForm(): JSX.Element {
-  const encryptedText = useStore(encryptedTextStore);
-  const displayEncryptionResult = useStore(displayEncryptionResultStore);
-  const hasCopyButtonBeenClicked = useStore(copyButtonClickedStore);
-  const encryptButtonDisabled = useStore(encryptButtonDisabledStore);
+  const [encryptedText, setEncryptedText] = useState("");
+  const [textToEncrypt, setTextToEncrypt] = useState("");
+  const [shouldDisplayEncryptionResult, setDisplayEncryptionResult] = useState(false);
+  const [hasCopyButtonBeenClicked, setHasCopyButtonBeenClicked] = useState(false);
+  const [isEncryptButtonDisabled, setEncryptButtonDisabled] = useState(true);
+  const [encryptedTextInputElement, setEncryptedTextInputElement] = useState<Input | null>(null);
+
+  const encryptionResultMessageDismissed = useCallback(() => setDisplayEncryptionResult(false), []);
+
+  const handleEncryptButtonClicked = useCallback(() => {
+    bcrypt({
+      password: textToEncrypt,
+      costFactor: 10,
+      salt: window.crypto.getRandomValues(new Uint8Array(16)),
+      outputType: "encoded",
+    }).then(setEncryptedText);
+  }, [textToEncrypt]);
+
+  const handleCopyButtonClicked = useCallback(() => {
+    if (encryptedTextInputElement === null) return;
+
+    const inputElement = (encryptedTextInputElement as unknown as { inputRef: { current: HTMLInputElement } }).inputRef
+      .current;
+
+    inputElement.select();
+    inputElement.setSelectionRange?.(0, inputElement.value.length);
+    document.execCommand("copy");
+
+    setHasCopyButtonBeenClicked(true);
+  }, [encryptedTextInputElement]);
+
+  useEffect(() => {
+    setHasCopyButtonBeenClicked(false);
+    setDisplayEncryptionResult(encryptedText.length > 0);
+  }, [encryptedText]);
+
+  useEffect(() => {
+    setDisplayEncryptionResult(false);
+    setEncryptButtonDisabled(textToEncrypt.length === 0);
+  }, [textToEncrypt]);
 
   return (
     <>
@@ -81,7 +52,7 @@ export function EncryptionForm(): JSX.Element {
       <Input
         type="text"
         placeholder="Enter some text to encrypt"
-        onChange={(_, data) => textToEncryptUpdated(data.value)}
+        onChange={(_, data) => setTextToEncrypt(data.value)}
         data-test-id="text-to-encrypt"
         fluid
         action
@@ -89,24 +60,24 @@ export function EncryptionForm(): JSX.Element {
         <input />
         <Button
           color="blue"
-          onClick={() => encryptButtonClicked()}
-          disabled={encryptButtonDisabled}
+          onClick={handleEncryptButtonClicked}
+          disabled={isEncryptButtonDisabled}
           data-test-id="button-to-encrypt"
         >
           Encrypt
         </Button>
       </Input>
-      {displayEncryptionResult && (
-        <Message onDismiss={() => encryptionResultMessageDismissed()} info>
+      {shouldDisplayEncryptionResult && (
+        <Message onDismiss={encryptionResultMessageDismissed} info>
           <Message.Header>Result:</Message.Header>
           <Input
             action={
-              <Button color="teal" icon labelPosition="right" onClick={() => copyButtonClicked()}>
+              <Button color="teal" icon labelPosition="right" onClick={() => handleCopyButtonClicked()}>
                 {hasCopyButtonBeenClicked ? "Copied!" : "Copy"} <Icon fitted name="copy" />
               </Button>
             }
             value={encryptedText}
-            ref={copyInputReferenceUpdated}
+            ref={setEncryptedTextInputElement}
             data-test-id="encrypted-text"
             fluid
           />
--- src/components/HashVerificationForm.tsx
+++ src/components/HashVerificationForm.tsx
@@ -1,53 +1,34 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Input, Button, Message, Icon } from "semantic-ui-react";
-import { useStore } from "effector-react";
-import type { BcryptVerifyOptions } from "hash-wasm";
 import { bcryptVerify } from "hash-wasm";
-import { restore, createEvent, createStore, createEffect, sample, combine } from "effector";
-
-const verify = createEffect((options: BcryptVerifyOptions) => bcryptVerify(options));
-
-const validateButtonClicked = createEvent();
-
-const validationResultMessageDismissed = createEvent();
-
-const textToValidateUpdated = createEvent<string>();
-
-const hashToValidateUpdated = createEvent<string>();
-
-const textToValidateStore = restore(textToValidateUpdated, "");
-
-const hashToValidateStore = restore(hashToValidateUpdated, "");
-
-const validationMatchedStore = restore(verify.doneData, false);
-
-const displayValidationResultStore = createStore(false)
-  .on(validateButtonClicked, () => true)
-  .reset([validationResultMessageDismissed, textToValidateStore.updates, hashToValidateStore.updates]);
-
-const isHashToValidateValidStore = hashToValidateStore.map((hashToValidate) => hashToValidate.startsWith("$"));
-
-const validateButtonDisabledStore = combine({
-  textToValidate: textToValidateStore,
-  hashToValidate: hashToValidateStore,
-  isHashToValidateValid: isHashToValidateValidStore,
-}).map(
-  ({ textToValidate, hashToValidate, isHashToValidateValid }) =>
-    textToValidate.length === 0 || hashToValidate.length === 0 || !isHashToValidateValid
-);
-
-sample({
-  clock: validateButtonClicked,
-  source: combine({ password: textToValidateStore, hash: hashToValidateStore }),
-  target: verify,
-});
 
 export function HashVerificationForm(): JSX.Element {
-  const hashToValidate = useStore(hashToValidateStore);
-  const displayValidationResult = useStore(displayValidationResultStore);
-  const isHashToValidateValid = useStore(isHashToValidateValidStore);
-  const validateButtonDisabled = useStore(validateButtonDisabledStore);
-  const validationMatched = useStore(validationMatchedStore);
+  const [hashToValidate, setHashToValidate] = useState("");
+  const [textToValidate, setTextToValidate] = useState("");
+  const [shouldDisplayValidationResult, setDisplayValidationResult] = useState(false);
+  const [isHashToValidateValid, setIsHashToValidateValid] = useState(false);
+  const [isValidateButtonDisabled, setValidateButtonDisabled] = useState(false);
+  const [isValidationMatching, setValidationMatching] = useState(false);
+
+  const handleValidationResultMessageDismissed = useCallback(() => setDisplayValidationResult(false), []);
+
+  const handleValidateButtonClicked = useCallback(() => {
+    bcryptVerify({ password: textToValidate, hash: hashToValidate }).then((isMatching) => {
+      setValidationMatching(isMatching);
+      setDisplayValidationResult(true);
+    });
+  }, [textToValidate, hashToValidate]);
+
+  useEffect(() => setDisplayValidationResult(false), [textToValidate]);
+
+  useEffect(() => {
+    setIsHashToValidateValid(hashToValidate.startsWith("$"));
+    setDisplayValidationResult(false);
+  }, [hashToValidate]);
+
+  useEffect(() => {
+    setValidateButtonDisabled(textToValidate.length === 0 || hashToValidate.length === 0 || !isHashToValidateValid);
+  }, [textToValidate, hashToValidate, isHashToValidateValid]);
 
   return (
     <>
@@ -58,40 +39,40 @@ export function HashVerificationForm(): JSX.Element {
         type="text"
         error={hashToValidate.length > 0 && !isHashToValidateValid}
         placeholder="Enter the hash to check"
-        onChange={(_, data) => hashToValidateUpdated(data.value)}
+        onChange={(_, data) => setHashToValidate(data.value)}
         data-test-id="hash-to-validate"
         fluid
       />
       <Input
         type="text"
         placeholder="Enter the text to check against"
-        onChange={(_, data) => textToValidateUpdated(data.value)}
+        onChange={(_, data) => setTextToValidate(data.value)}
         data-test-id="text-to-validate"
         fluid
       />
       <Button
         color="blue"
-        onClick={() => validateButtonClicked()}
-        disabled={validateButtonDisabled}
+        onClick={handleValidateButtonClicked}
+        disabled={isValidateButtonDisabled}
         data-test-id="button-to-validate"
         fluid
       >
         Check if hash and text match
       </Button>
-      {displayValidationResult && validationMatched && (
+      {shouldDisplayValidationResult && isValidationMatching && (
         <Message
           header="Result:"
           content="Hash and text match!"
-          onDismiss={() => validationResultMessageDismissed()}
+          onDismiss={handleValidationResultMessageDismissed}
           data-test-id="validation-result"
           success
         />
       )}
-      {displayValidationResult && !validationMatched && (
+      {shouldDisplayValidationResult && !isValidationMatching && (
         <Message
           header="Result:"
           content="Hash and text don't match."
-          onDismiss={() => validationResultMessageDismissed()}
+          onDismiss={handleValidationResultMessageDismissed}
           negative
         />
       )}
