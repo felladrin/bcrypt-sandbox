0e7499838ee47c6e6d42f050b0bb3169aed2a33b
Victor Nogueira <felladrin@gmail.com>
2022-09-03 19:09:38 +0000

    Replace Cypress by Playwright

---
+++ tests/actions.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("encryption and validation of a random text", async ({ page }) => {
+  await page.goto("/");
+
+  const randomText = Math.random().toString(36).substring(2);
+
+  const textToEncryptElement = page.locator('[data-test-id="text-to-encrypt"]').locator("input");
+
+  await textToEncryptElement.type(randomText);
+
+  await expect(textToEncryptElement).toHaveValue(randomText);
+
+  await page.locator('[data-test-id="button-to-encrypt"]').click();
+
+  const encryptedText = await page.locator('[data-test-id="encrypted-text"]').locator("input").inputValue();
+
+  await page.locator('[data-test-id="hash-to-validate"]').locator("input").type(encryptedText);
+
+  await page.locator('[data-test-id="text-to-validate"]').locator("input").type(randomText);
+
+  await page.locator('[data-test-id="button-to-validate"]').click();
+
+  await expect(page.locator('[data-test-id="validation-result"]')).toHaveClass(/success/);
+});
