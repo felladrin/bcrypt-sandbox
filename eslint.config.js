6539042fdf6e7933f0c82e98dfc30568e4ef2747
Victor Nogueira <felladrin@gmail.com>
2024-04-13 01:06:02 +0300

    Update lint script

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import pluginJs from "@eslint/js";
 import pluginTs from "typescript-eslint";
 
 export default [
-  { ignores: ["dist"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...pluginTs.configs.recommended,
